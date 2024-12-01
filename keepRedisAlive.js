a0948a67ea82345d8682b7aab9dfe54ae779862a
BeringeloF <sasorim54@gmail.com>
2024-12-01 12:48:55 -0300

    updating the keepRedisAlive file

--- keepRedisAlive.js
+++ keepRedisAlive.js
@@ -1,19 +1,28 @@
 import Redis from 'ioredis';
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST, // Host do Redis
-  port: process.env.REDIS_PORT, // Porta do Redis
-  password: process.env.REDIS_PASSWORD, // Senha do Redis (se necessário)
-  tls: process.env.REDIS_TLS === 'true' ? {} : undefined, // Para conexões seguras
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+  tls: process.env.REDIS_TLS === 'true' ? {} : undefined,
+  connectTimeout: 10000,
+  readTimeout: 10000,
 });
 
 redis.on('error', (err) => console.log(err));
 
 async function keepAlive() {
-  await redis.set('keep_alive', 'alive');
-  const val = await redis.get('keep_alive');
-  console.log(val);
-  await redis.del('keep_alive');
+  try {
+    await redis.set('keep_alive', 'alive');
+    const val = await redis.get('keep_alive');
+    console.log('Redis value:', val);
+    await redis.del('keep_alive');
+  } catch (err) {
+    console.error('Error in keepAlive:', err);
+  } finally {
+    // Garantir desconexão do Redis para evitar que o script fique "pendurado"
+    redis.disconnect();
+  }
 }
 
 keepAlive();
